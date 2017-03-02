75bb395f82550dbe63e24b110f6bd8ea82f9d54b
Christian Fei <crifei93@gmail.com>
2017-03-02 21:58:20 +0100

    make uat pass

--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
-// const { ok } = require('assert')
+const { equal } = require('assert')
+
+const benchmark = require('../')
 
 test('usage', () => {
-  test.skip('exports a function')
+  test('exports a function', () => {
+    equal(typeof benchmark, 'function')
+  })
 })
