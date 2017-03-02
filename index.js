74d36d5e06e347573d85118e5e4d99c91a2535c6
Christian Fei <crifei93@gmail.com>
2017-03-02 22:20:06 +0100

    refactor

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const { test } = require('m.test')
 const { ok } = require('assert')
 
-module.exports = function kissBenchmark (fastBenchmark, slowBenchmark, maxRuns, failureMessage) {
-  maxRuns = parseInt(maxRuns || process.env.MAX_RUNS || 1000000)
+const defaultMaxRuns = 1000000
+
+module.exports = function kissBenchmark (fastBenchmark, slowBenchmark, options = {}, failureMessage) {
+  const maxRuns = parseInt(options.maxRuns || process.env.MAX_RUNS || defaultMaxRuns)
   test('comparison-benchmark', () => {
-    const startFastBenchmarkHrTime = process.hrtime()
-    for (let i = 0; i < maxRuns; i++) {
-      slowBenchmark()
-    }
-    const endFastBenchmarkHrTime = process.hrtime(startFastBenchmarkHrTime)
-
-    const startSlowBenchmarkHrTime = process.hrtime()
-    for (let i = 0; i < maxRuns; i++) {
-      fastBenchmark()
-    }
-    const endSlowBenchmarkHrTime = process.hrtime(startSlowBenchmarkHrTime)
-
+    const [startFastBenchmarkHrTime, endFastBenchmarkHrTime] = bench(fastBenchmark, {maxRuns})
+    const [startSlowBenchmarkHrTime, endSlowBenchmarkHrTime] = bench(slowBenchmark, {maxRuns})
     const fastBenchmarkDurationInNanos = startFastBenchmarkHrTime[0] * 1e9 + endFastBenchmarkHrTime[1]
     const slowBenchmarkDurationInNanos = fastBenchmarkDurationInNanos - startSlowBenchmarkHrTime[0] * 1e9 + endSlowBenchmarkHrTime[1]
 
     console.log(`Benchmark took ${startFastBenchmarkHrTime[0] * 1e9 + endSlowBenchmarkHrTime[1]} nanoseconds`)
     ok(slowBenchmarkDurationInNanos < fastBenchmarkDurationInNanos, failureMessage)
   })
+}
+
+function bench (fn, options = {}) {
+  const {maxRuns} = options
+  const startHrTime = process.hrtime()
+  for (let i = 0; i < maxRuns; i++) { fn() }
+  return [startHrTime, process.hrtime(startHrTime)]
 }
--- test/index.js
+++ test/index.js
@@ -1,19 +1,20 @@
-const { ok, equal, notThrows, throws } = require('assert')
+const { equal, notThrows, throws } = require('assert')
 
 const benchmark = require('../')
 const slowBenchmark = () => { return Math.random() * 42 * Math.ceil(Math.PI * Math.random()) }
 const fastBenchmark = () => 42
 
+const benchmarkOptions = {maxRuns: 10000}
 test('usage', () => {
   test('exports a function', () => {
     equal(typeof benchmark, 'function')
   })
 
   test('does not throw when assumption is true', () => {
-    notThrows(benchmark(fastBenchmark, slowBenchmark, 10000), 'my assumption is FALSE')
+    notThrows(benchmark(fastBenchmark, slowBenchmark, benchmarkOptions), 'my assumption is FALSE')
   })
 
   test('throws when assumption is false', () => {
-    throws(() => benchmark(slowBenchmark, fastBenchmark, 10000, 'my assumption is FALSE'), Error)
+    throws(() => benchmark(slowBenchmark, fastBenchmark, benchmarkOptions, 'my assumption is FALSE'), Error)
   })
 })
