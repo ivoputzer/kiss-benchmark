4573ab95bdf9083ca8394927a7b0843a5bcd30f0
Christian Fei <crifei93@gmail.com>
2017-03-02 22:08:23 +0100

    add uat

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = function kissBenchmark () {
-  
+const { test } = require('m.test')
+const { ok } = require('assert')
+
+module.exports = function kissBenchmark (fastBenchmark, slowBenchmark, maxRuns, failureMessage) {
+  maxRuns = parseInt(maxRuns || process.env.MAX_RUNS || 1000000)
+  test('comparison-benchmark', () => {
+    const startFastBenchmarkHrTime = process.hrtime()
+    for (let i = 0; i < maxRuns; i++) {
+      slowBenchmark()
+    }
+    const endFastBenchmarkHrTime = process.hrtime(startFastBenchmarkHrTime)
+
+    const startSlowBenchmarkHrTime = process.hrtime()
+    for (let i = 0; i < maxRuns; i++) {
+      fastBenchmark()
+    }
+    const endSlowBenchmarkHrTime = process.hrtime(startSlowBenchmarkHrTime)
+
+    const fastBenchmarkDurationInNanos = startFastBenchmarkHrTime[0] * 1e9 + endFastBenchmarkHrTime[1]
+    const slowBenchmarkDurationInNanos = fastBenchmarkDurationInNanos - startSlowBenchmarkHrTime[0] * 1e9 + endSlowBenchmarkHrTime[1]
+
+    console.log(`Benchmark took ${startFastBenchmarkHrTime[0] * 1e9 + endSlowBenchmarkHrTime[1]} nanoseconds`)
+    ok(slowBenchmarkDurationInNanos < fastBenchmarkDurationInNanos, failureMessage)
+  })
 }
--- test/index.js
+++ test/index.js
@@ -1,9 +1,19 @@
-const { equal } = require('assert')
+const { ok, equal, notThrows, throws } = require('assert')
 
 const benchmark = require('../')
+const slowBenchmark = () => { return Math.random() * 42 * Math.ceil(Math.PI * Math.random()) }
+const fastBenchmark = () => 42
 
 test('usage', () => {
   test('exports a function', () => {
     equal(typeof benchmark, 'function')
+  })
+
+  test('does not throw when assumption is true', () => {
+    notThrows(benchmark(fastBenchmark, slowBenchmark, 10000), 'my assumption is FALSE')
+  })
+
+  test('throws when assumption is false', () => {
+    throws(() => benchmark(slowBenchmark, fastBenchmark, 10000, 'my assumption is FALSE'), Error)
   })
 })
